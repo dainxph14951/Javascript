067be141babf8ffbfb57d28d7239b1b9d4776fc0
daibede <xuandai300092@gmail.com>
2022-01-09 21:45:59 +0700

    anh

--- src/comboudun/header.js
+++ src/comboudun/header.js
@@ -2,7 +2,7 @@ const header = {
     render() {
         return /* html */`<div class="mb-2">
                 <div class="justify-between items-center max-w-5xl py-4 bg-blue-800 mx-auto">
-                <img src="../images/logo.png" class="mx-auto" />
+                <img src="https://picsum.photos/200/300" class="mx-auto" />
                 </div>
                 <div class="flex justify-between items-center max-w-5xl py-3 bg-orange-600 mx-auto">
                 <nav>
