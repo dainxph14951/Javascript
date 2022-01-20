cee9ff436de1e9c2751817600e4cb6c5027323a0
daibede <xuandai300092@gmail.com>
2022-01-20 10:44:09 +0700

    updata

--- src/pages/client/home.js
+++ src/pages/client/home.js
@@ -15,13 +15,13 @@ const homePage = {
                 <div class="grid grid-cols-3 gap-8">
                     ${data.map((post) => `
                    
-                        <div class="border p-4">
+                        <div class="border p-4 text-center">
                         
                             <a href="/products/${post.id}">
                                 <img class="mx-auto" src="${post.img}" alt="" />
                             </a>
-                            <a href="/products/${post.id}">
-                            <h3 class="my-3 font-semibold text-lg text-orange-500">${post.title}</h3>
+                            <a href="/products/${post.id}" class="no-underline">
+                            <h3 class="my-3 font-semibold text-lg text-orange-500 ">${post.title}</h3>
                             </a>
                             <p>${post.desc}</p>
                           
