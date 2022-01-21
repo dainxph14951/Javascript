3642cd814a8e1873cdd2c32fe6011d2a55bd686a
daibede <xuandai300092@gmail.com>
2022-01-21 16:44:36 +0700

    uodate chi tiet sp

--- src/pages/client/blog.js
+++ src/pages/client/blog.js
@@ -1,8 +1,8 @@
 const Blog = {
     render() {
-        return `<div class="max-w-5xl mx-auto">
+        return /* html */`<div class="max-w-5xl mx-auto">
         <h1>Trang Blog</h1>
-        </div>`;
+ `;
     },
 };
 export default Blog;
--- src/pages/client/contact.js
+++ src/pages/client/contact.js
@@ -2,6 +2,7 @@ const Contact = {
     render() {
         return `<div class="max-w-5xl mx-auto">
         <h1>Trang Liên Hệ</h1>
+            
         </div>`;
     },
 };
